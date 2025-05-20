a44217fcb3e14f4e30f173d6f5d1ae4ea8ed63a6
Felipe Peronica <felipeperonica@gmail.com>
2025-05-20 16:40:37 -0300

    :sparkles: Criação do Recurso Tema e Relacionamento com a Classe Postagem

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,60 @@
+package com.generation.blogpessoal.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema") //CREATE TABLE tb_postagens();
+public class Tema {
+	
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+	private Long id;
+	
+	@Column(length = 1000)
+	@NotBlank(message = "O atributo texto é obrigatorio!")
+	@Size(min = 10, max = 1000, message = "O atributo texto dever ter no minimo 10 e no maximo 1000 caracteres.")
+	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+			
+}
